d067a30075cee105f90034589fd8ffcd602b3de5
olivebishop <olivehendrilgen1@gmail.com>
2024-03-23 23:37:46 +0300

    end of if else lesson

--- ifElse.go
+++ ifElse.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("if else")
+	// manimum age for you to come to my party is 25 years and above
+	// the following shows if else statement / conditions of friends and geusts invited to my party
+	age := 18
+
+	if age >= 25 {
+		fmt.Println("you are allowed to the party")
+
+	} else if age >= 20 {
+		fmt.Println("yes you are a teen but we need above 25 years and above  dude ! HAHA")
+
+	} else {
+		fmt.Println("under age are not allowed !")
+	}
+
 }
