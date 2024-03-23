7e9d6b46fe1737e8f437edb2b7e87c36515c7322
olivebishop <olivehendrilgen1@gmail.com>
2024-03-23 21:19:33 +0300

    end of varibales lesson

---
+++ basics/operators.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// arithmetic operators
+	//examples of arithemtics :
+	// + - / * %
+	var num1 = 2
+	var num2 = 5
+	var sum = num1 + num2
+	fmt.Println(sum)
+
+}
