5b9a057548c1926ac279a6c42b424edf204c9248
olivebishop <olivehendrilgen1@gmail.com>
2024-03-23 22:35:49 +0300

    end of lesson

--- basics/operators.go
+++ basics/operators.go
@@ -11,16 +11,38 @@ func main() {
 	var sum = num1 + num2
 	var minus = num1 - num2
 	var divide = num1 / num2
-	var multiplicaion = num1 * num2
+	var multiplication = num1 * num2
 
 	var remainder = num1 % num2
 
-	fmt.Println(sum, minus, divide, multiplicaion, remainder)
+	fmt.Println(sum, minus, divide, multiplication, remainder)
 
 	// relational operators
 
 	var result = num1 > num2
+	var isTrue = num2 > num1
+	var answer = num1 != num2
 
-	fmt.Println(result)
+	fmt.Println(result, isTrue, answer)
+
+	//logical operators
+	//invite guests to my party
+
+	var name = "olive"
+	var age = 25
+
+	// i want strickly only those with  names like my name  and those who are above 25 to come to my party..var
+
+	var invitedToParty = name == "olive" && age > 25
+	fmt.Println(invitedToParty)
+
+	// i want any other dude with diffrent name but only age should be above 25 ( using or which is ||)
+	var inviteToParty = name == "olive" || age > 25
+	fmt.Println(inviteToParty)
+
+	// i dont want to be invuted on my own party coz am boring LOL haha !
+
+	var invitesToParty = !(name == "olive") && (age >= 25 && age < 90)
+	fmt.Println(invitesToParty)
 
 }
